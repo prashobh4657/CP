2c9a9e33ef60ae53963c666a9663a7db413f3f74
Prashobh <81906847+prashobh4657@users.noreply.github.com>
2022-12-11 17:31:19 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -7,7 +7,22 @@ using namespace std;
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);
 
-
+/*
+        vector<int> prefix(n);
+        prefix[0]=a[0];
+        for(int i=1;i<n;i++)
+        prefix[i]=prefix[i-1]+a[i];
+        
+        vector<int> suffix(n);
+        suffix[n-1]=a[n-1];
+        for(int i=n-2;i>=0;i--)
+        suffix[i]=suffix[i+1]+a[i];
+        
+        for(int i=0;i<n;i++)
+        cout<<prefix[i]<<" ";cout<<endl;
+        for(int i=0;i<n;i++)
+        cout<<suffix[i]<<" ";cout<<endl;
+*/
 bool isvowel(char z)
 {
     z=tolower(z);  
