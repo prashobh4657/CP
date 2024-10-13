3a8341596fc90e4adf8a0c5708b0781edeffd980
Prashobh <81906847+prashobh4657@users.noreply.github.com>
2024-10-13 16:18:18 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -185,6 +185,22 @@ vector<int> primeFactors(int n) //isPrime() also required;
     }
     return v;
 }
+bool binarysearch(vector<int> &a,int target)
+{
+    int low=0; int high=a.size()-1;
+    while(low<=high)
+    {
+        int mid=low+(high-low)/2;
+        if(a[mid]==target)
+            return true;
+        else if(a[mid]<target)
+            low=mid+1;
+        else
+            high=mid-1;
+    }
+    return false;
+}
+
 
 /* To build n*64 where v[i] denotes 64-bit representation of a[i]
 vector<vector<int>> v(n, vector<int>(64, 0));
