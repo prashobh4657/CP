26c465884a7fbbadd2d697cc01859f649279993a
Prashobh <81906847+prashobh4657@users.noreply.github.com>
2026-07-11 13:29:45 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -474,8 +474,25 @@ vector<int> prefix_function(string &s) // O(n);
     }
     return pi;
 }
-
-
+vector<int> z_function(string &s) // O(n)
+{
+    int n = s.size();
+    vector<int> z(n);
+    int l = 0, r = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (i <= r)
+            z[i] = min(r - i + 1, z[i - l]);
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]])
+            z[i]++;
+        if (i + z[i] - 1 > r)
+        {
+            l = i;
+            r = i + z[i] - 1;
+        }
+    }
+    return z;
+}
 int32_t main()
 {
     fio;
